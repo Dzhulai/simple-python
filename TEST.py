8b2280c6bb629cee4df4571d95713353eb906a0a
dvt <Dzhulai>
2015-03-31 16:58:12 +0300

    inc-test  classes

--- TEST.py
+++ TEST.py
@@ -588,7 +588,242 @@ apple.is_edible()
 
 # ---------------------------------------------------------------------
 
+# Class definition
+class Animal(object):
+    """Makes cute animals."""
+    # For initializing our instance objects
+    def __init__(self, name, age, is_hungry):
+        self.name = name
+        self.age = age
+        self.is_hungry = is_hungry
+# Note that self is only used in the __init__()
+# function definition; we don't need to pass it
+# to our instance objects.
 
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
+
+print zebra.name, zebra.age, zebra.is_hungry
+print giraffe.name, giraffe.age, giraffe.is_hungry
+print panda.name, panda.age, panda.is_hungry
+
+# ---------------------------------------------------------------------
+
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+zebra = Animal("Jeffrey", 2)
+giraffe = Animal("Bruce", 1)
+panda = Animal("Chad", 7)
+
+print zebra.name, zebra.age, zebra.is_alive
+print giraffe.name, giraffe.age, giraffe.is_alive
+print panda.name, panda.age, panda.is_alive
+
+# ---------------------------------------------------------------------
+
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    # Add your method he
+    def description(self):
+        print self.name
+        print self.age
+    
+hippo = Animal("Chad", 7)
+hippo.description()
+
+# ---------------------------------------------------------------------
+
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    health   = "good"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+     
+    # Add your method he
+    def description(self):
+        print self.name
+        print self.age
+sloth = Animal("sloth", 4)
+ocelot = Animal("ocelot", 5)
+hippo = Animal("Chad", 7)
+ocelot.description()
+print ocelot.health
+sloth.description()
+print sloth.health
+hippo.description()
+print hippo.health
+
+# ---------------------------------------------------------------------
+
+class ShoppingCart(object):
+    """Creates shopping cart objects
+    for users of our fine website."""
+    items_in_cart = {}
+    def __init__(self, customer_name):
+        self.customer_name = customer_name
+        
+    def add_item(self, product, price):
+        """Add product to the cart."""
+        if not product in self.items_in_cart:
+            self.items_in_cart[product] = price
+            print product + " added."
+        else:
+            print product + " is already in the cart."
+
+    def remove_item(self, product):
+        """Remove product from the cart."""
+        if product in self.items_in_cart:
+            del self.items_in_cart[product]
+            print product + " removed."
+        else:
+            print product + " is not in the cart."
+
+my_cart = ShoppingCart("Eric")
+my_cart.add_item("UuA", 10)
+my_cart.items_in_cart
+
+# ---------------------------------------------------------------------
+
+class Customer(object):
+    """Produces objects that represent customers."""
+    def __init__(self, customer_id):
+        self.customer_id = customer_id
+
+    def display_cart(self):
+        print "I'm a string that stands in for the contents of your shopping cart!"
+
+class ReturningCustomer(Customer):
+    """For customers of the repeat variety."""
+    def display_order_history(self):
+        print "I'm a string that stands in for your order history!"
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
+
+
+
+# ---------------------------------------------------------------------
+
+class Triangle(object):
+    number_of_sides = 3
+    def __init__(self, angle1, angle2, angle3 ):
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
+    def check_angles(self):
+        if self.angle1+self.angle2+self.angle3==180:
+            return True
+        else:
+            return False
+
+class Equilateral(Triangle):
+    angle = 60
+    def __init__(self):
+        self.angle1=self.angle
+        self.angle2=self.angle
+        self.angle3=self.angle
+
+my_triangle = Triangle(90,30,60)
+print my_triangle.number_of_sides
+print my_triangle.check_angles()
+
+# ---------------------------------------------------------------------
+
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+        
+my_car = Car("DeLorean", "silver", 88)
+print my_car.condition
+print my_car.model
+print my_car.color
+print my_car.mpg  
+
+# ---------------------------------------------------------------------
+
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+    def display_car(self):
+        print "This is a %s %s with %i MPG." % (self.color, self.model, self.mpg)
+
+my_car = Car("DeLorean", "silver", 88)
+
+print my_car.display_car()
+
+# ---------------------------------------------------------------------
+
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+    def display_car(self):
+        print "This is a %s %s with %i MPG." % (self.color, self.model, self.mpg)
+    def drive_car(self):
+        self.condition = "used"
+        return self.condition
+
+my_car = Car("DeLorean", "silver", 88)
+print my_car.condition
+
+my_car.drive_car()
+print my_car.condition
+
+
+# ---------------------------------------------------------------------
+
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+    def display_car(self):
+        print "This is a %s %s with %i MPG." % (self.color, self.model, self.mpg)
+    def drive_car(self):
+        self.condition = "used"
+        return self.condition
+my_car = Car("DeLorean", "silver", 88)
+print my_car.condition
+my_car.drive_car()
+print my_car.condition
+
+
+class ElectricCar(Car):
+    def __init__(self, model, color, mpg, battery_type):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+        self.battery_type = battery_type
+    def drive_car(self):
+        self.condition = "like new"
+
+my_car = ElectricCar("DeLorean", "silver", 88, "molten salt")
+print my_car.condition
+
+my_car.drive_car()
+print my_car.condition
 
 # ---------------------------------------------------------------------
 
@@ -611,6 +846,9 @@ apple.is_edible()
 
 
 # ---------------------------------------------------------------------
+
+
+
 
 
 
